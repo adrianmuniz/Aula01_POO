72501ce8784325d2a2b499285fec597d3373742f
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-16 21:22:36 -0300

    Importando a classe de outro pacote

--- src/implementacao/implementarCarro.java
+++ src/implementacao/implementarCarro.java
@@ -1,10 +1,17 @@
 package implementacao;
 
+import beans.CarroFormula1;
+
 public class implementarCarro {
 
 	public static void main(String[] args) {
-		System.out.println("Saida de texto");
-
+		CarroFormula1 carro = new CarroFormula1();
+		
+		carro.equipe = "ferrari";
+		
+		System.out.println("Carro-Equipe " + carro.equipe);
+		
+		
 	}
 
 }
