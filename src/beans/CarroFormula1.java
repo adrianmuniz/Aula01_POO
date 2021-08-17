da84dffdbe20aac7c98e513b289156ba305322ac
Cainã Muniz <adrianmuniz.ceo@outlook.com>
2021-08-16 21:11:10 -0300

    Criando a classe de implementação e testando

--- src/beans/CarroFormula1.java
+++ src/beans/CarroFormula1.java
@@ -1,5 +1,11 @@
 package beans;
 
 public class CarroFormula1 {
-
+	//[visibilidade] [datatype] propiedade
+	
+	private String cor;
+	public String equipe;
+	private int velocidade;
+	private float valor;
+	
 }
---
+++ src/implementacao/implementarCarro.java
@@ -0,0 +1,10 @@
+package implementacao;
+
+public class implementarCarro {
+
+	public static void main(String[] args) {
+		System.out.println("Saida de texto");
+
+	}
+
+}
